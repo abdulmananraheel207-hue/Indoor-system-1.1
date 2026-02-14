142f596123186470c01af066333f90c4df07a3b0
a62659246-spec <a62659246@gmail.com>
2026-02-14 23:43:47 +0500

    ..

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,7 @@ module.exports = {
   CORS_ORIGIN: process.env.CORS_ORIGIN || "http://localhost:5173",
   DB_HOST: process.env.DB_HOST || "localhost",
   DB_USER: process.env.DB_USER || "root",
-  DB_PASSWORD: process.env.DB_PASSWORD || "password1144",
+  DB_PASSWORD: process.env.DB_PASSWORD || "ahmed.147",
   DB_DATABASE: process.env.DB_DATABASE || "indoor_system",
   JWT_SECRET:
     process.env.JWT_SECRET ||
